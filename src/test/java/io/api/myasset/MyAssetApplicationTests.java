fef201810aab55fb15e69d4f1cd7c78f84190854
kdhdd <kdso10@naver.com>
2026-04-14 13:58:13 +0900

    feat: initial commit

---
+++ src/test/java/io/api/myasset/MyAssetApplicationTests.java
@@ -0,0 +1,13 @@
+package io.api.myasset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAssetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
