8e1c12ba4f2f4a6e210b6ac8c98dd07400387bd5
hyeonkangkimm <kkhk75@gmail.com>
2026-04-17 17:46:53 +0900

    Refactor: MissionAcceptResponse 수정

--- src/main/java/io/api/myasset/domain/mission/dto/MissionAcceptResponse.java
+++ src/main/java/io/api/myasset/domain/mission/dto/MissionAcceptResponse.java
@@ -1,6 +1,11 @@
 package io.api.myasset.domain.mission.dto;
 
 public record MissionAcceptResponse(
-        Long missionId
+        Long missionId,
+        String title,
+        String description,
+        String iconType,
+        Integer rewardPoint,
+        Integer expectedSavingAmount
 ) {
 }
